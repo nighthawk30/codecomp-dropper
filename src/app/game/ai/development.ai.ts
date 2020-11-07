d32df01c8ba6b3bcd257c40755020a5e876678a2
nighthawk30 <nathan4taylor@gmail.com>
2020-11-07 16:54:57 -0700

    Update development.ai.ts

--- src/app/game/ai/development.ai.ts
+++ src/app/game/ai/development.ai.ts
@@ -48,7 +48,70 @@ export type Coord = [number, number];
 */
 
 // do not include "export const developmentScript = " with your submission
-export const developmentScript = `
+export const developmentScript = 
+
+`
+//global variables
+let turn = -1;
+
+function main(gameState, side)
+{
+  //you can move somewhere if it is both on the board and the tilestate > 1
+  const myTeam = gameState.teamStates[side];
+  const [rowSize, colSize] = gameState.boardSize;
+  const boardLayout[][] = gameState.tileStates;
+  const possibleMoves = [];
+  turn++;
+
+  //board value
+  let board = [];
+  for (let i = 0; i < )
+
+  return new Promise((resolve, reject) => {
+    const callback = () => resolve(
+      myTeam.reduce((moveSet, member) => {
+        
+        //THE CODE PER MONSTER
+
+        if (member.isDead)
+        {
+          moveSet.push('none');
+        }
+        else
+        {
+          possibleMoves.push('none');
+          //where the monster is
+          const [row, col] = member.coord;
+          if (row > 1 && boardLayout[row - 1][col] > 1) 
+          {
+            possibleMoves.push('north');
+          }
+          if (row < rowSize - 1 && boardLayout[row + 1][col] > 1) 
+          {
+            possibleMoves.push('south');
+          }
+          if (col > 1 && boardLayout[row][col - 1] > 1) 
+          {
+            possibleMoves.push('west');
+          }
+          if (col < colSize - 1 && boardLayout[row][col + 1] > 1) 
+          {
+            possibleMoves.push('east');
+          }
+          moveSet.push(possibleMoves[Math.floor(Math.random() * possibleMoves.length)]);
+          possibleMoves.length = 0;
+        }
+        return moveSet;
+      }, [])
+    );
+    return callback();
+}
+
+
+`;
+
+
+/*`
 //global variables
 let board = [];
 let turn = -1;
@@ -181,3 +244,4 @@ function main(gameState, side)
     })
 }
 `;
+*/
