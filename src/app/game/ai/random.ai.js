2058b7ce604f3bdc7c78e5d3c16b8ae2a098ffae
nighthawk30 <nathan4taylor@gmail.com>
2020-11-13 16:49:05 -0700

    Update random.ai.js

--- src/app/game/ai/random.ai.js
+++ src/app/game/ai/random.ai.js
@@ -1,10 +1,10 @@
 function main(gameState, side)
 {
   const myTeam = gameState.teamStates[side];
-  const possibleMoves = [];
   const [rowSize, colSize] = gameState.boardSize;
   let boardLayout = gameState.tileStates;//the strength of every tile on the board
-  
+
+
   return new Promise((resolve, reject) => {
     const callback = () => resolve(
       myTeam.reduce((moveSet, member) => {
@@ -15,28 +15,60 @@ function main(gameState, side)
         else 
         {
           const [row, col] = member.coord;
-          if (row > 0 && boardLayout[row - 1][col] > 1)
-          {
-            possibleMoves.push('north');
-          }
-          if (row < rowSize - 1 && boardLayout[row + 1][col] > 1)
-          {
-            possibleMoves.push('south');
-          }
-          if (col > 0 && boardLayout[row][col - 1] > 1)
-          {
-            possibleMoves.push('west');
-          }
-          if (col < colSize - 1 && boardLayout[row][col + 1] > 1)
-          {
-            possibleMoves.push('east');
-          }
-          if (boardLayout[row][col] > 1)
-          {
-            possibleMoves.push('none');
-          }
+          let direction = 'none';
+          let bestMove = 0;
+          let n = 0;
+          let s = 0;
+          let e = 0;
+          let w = 0;
+          let x = 0;
 
-          let move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+          if (locationExists(rpos, cpos - 1, rowSize, colSize, boardLayout))
+          {
+            n = countSpaces(rpos, cpos - 1, rowSize, colSize, boardLayout);
+            if (n >= bestMove)
+            {
+              direction = 'north';
+              bestMove = n;
+            }
+          }
+          if (locationExists(rpos, cpos + 1, rowSize, colSize, boardLayout))
+          {
+            s = countSpaces(rpos, cpos + 1, rowSize, colSize, boardLayout);
+            if (s >= bestMove)
+            {
+              direction = 'south';
+              bestMove = s;
+            }
+          }
+          if (locationExists(rpos - 1, cpos, rowSize, colSize, boardLayout))
+          {
+            e = countSpaces(rpos - 1, cpos, rowSize, colSize, boardLayout);
+            if (e >= bestMove)
+            {
+              direction = 'east';
+              bestMove = e;
+            }
+          }
+          if (locationExists(rpos + 1, cpos, rowSize, colSize, boardLayout))
+          {
+            w = countSpaces(rpos + 1, cpos, rowSize, colSize, boardLayout);
+            if (w >= bestMove)
+            {
+              direction = 'west';
+              bestMove = w;
+            }
+          }
+          if (locationExists(rpos, cpos, rowSize, colSize, boardLayout))
+          {
+            x = countSpaces(rpos, cpos, rowSize, colSize, boardLayout);
+            if (x >= bestMove)
+            {
+              direction = 'none';
+              bestMove = x;
+            }
+          }
+          let move = direction;
           //remove strength from chosen tile - so collisions dont occur
           switch(move)
           {
@@ -57,7 +89,6 @@ function main(gameState, side)
               break;
           }
           moveSet.push(move);
-          possibleMoves.length = 0;
         }
         return moveSet;
       }, [])
@@ -65,4 +96,82 @@ function main(gameState, side)
 
     callback();
   })
+}
+
+//recursively find the value of all tiles - start it at the most valuble tiles
+function valueRecursion(rpos, cpos, rowSize, colSize, boardLayout, tileValue)
+{
+  //check if there are
+  //for all surrounding tiles, if they are on the board
+  if (locationExists(rpos + 1, cpos, rowSize, colSize, boardLayout))
+  {
+    if (tileValue[rpos + 1][cpos] === 0)
+    {
+      tileValue[rpos + 1][cpos] = 1;
+      valueRecursion(rpos + 1, cpos, rowSize, colSize, boardLayout, tileValue);
+    }
+  }
+  if (locationExists(rpos - 1, cpos, rowSize, colSize, boardLayout))
+  {
+    if (tileValue[rpos - 1][cpos] === 0)
+    {
+      tileValue[rpos - 1][cpos] = 1;
+      valueRecursion(rpos - 1, cpos, rowSize, colSize, boardLayout, tileValue);
+    }
+  }
+  if (locationExists(rpos, cpos + 1, rowSize, colSize, boardLayout))
+  {
+    if (tileValue[rpos][cpos + 1] === 0)
+    {
+      tileValue[rpos][cpos + 1] = 1;
+      valueRecursion(rpos, cpos + 1, rowSize, colSize, boardLayout, tileValue);
+    }
+  }
+  if (locationExists(rpos, cpos - 1, rowSize, colSize, boardLayout))
+  {
+    if (tileValue[rpos][cpos - 1] === 0)
+    {
+      tileValue[rpos][cpos - 1] = 1;
+      valueRecursion(rpos, cpos - 1, rowSize, colSize, boardLayout, tileValue);
+    } 
+  }
+}
+
+function countSpaces(rpos, cpos, rowSize, colSize, boardLayout)
+{
+  let tileValue = [];
+  let spaceCount = 0;
+  //Build Tile Value board
+  for(let i = 0; i < rowSize; i++)
+  {
+    tileValue.push([]);
+    for(let j = 0; j < colSize; j++)
+    {
+      tileValue[i].push(0);//0 means it has not been visited
+    }
+  }
+  //Count tiles with 1
+  for (let i = 0; i < rowSize; i++)
+  {
+    for(let j = 0; j < colSize; j++)
+    {
+      if (tileValue[i][j] === 1)
+      {
+        spaceCount++;
+      }
+    }
+  }
+  return spaceCount;
+}
+
+function locationExists(rpos, cpos, rowSize, colSize, boardLayout)
+{
+  if (rpos >= 0 && rpos < rowSize && cpos >= 0 && cpos < colSize && boardLayout[rpos][cpos] > 1)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
